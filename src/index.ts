cb115541bd192f14780730465b97fc6069d3fca6
fluxtudione <190565672+fluxtudione@users.noreply.github.com>
2026-05-03 02:08:02 +0700

    feat: project setup

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { Elysia } from "elysia";
+import { db } from "./db";
+import { users } from "./db/schema";
+
+const app = new Elysia()
+  .get("/", () => "Hello Elysia!")
+  .get("/users", async () => {
+    try {
+      return await db.select().from(users);
+    } catch (error) {
+      console.error(error);
+      return { error: "Database connection failed. Please check your .env" };
+    }
+  })
+  .listen(3000);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
